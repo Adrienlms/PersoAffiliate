e08a20e6cac5d11ee906f13f90da43ef55523e78
Adrien <adrien.lms@gmail.com>
2021-01-26 18:32:49 +0100

    debut refonte

--- src/components/Cards/TinyCards.js
+++ src/components/Cards/TinyCards.js
@@ -4,11 +4,9 @@ class TinyCards extends React.Component {
 
     render() {
         return (
-            <div className="col-md-2 col-sm-12">
+            <div className="col-lg-2 col-md-12">
                 <div className="cardsino">
-                    <img className="c-img" src="" alt=""/>
-                    <h5 className="c-title"></h5>
-                    <p className="c-details"></p>
+                    <img className="c-img" src="/img/images.jpg" alt=""/>
                 </div>
             </div>
         );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,21 +6,26 @@ class NavBar extends React.Component {
     render() {
         return (
             <div className="col-md-12 col-sm-12 navbarCustom">
-                <nav className="offset-1 navbar navbar-expand-lg">
-                    <div className="container-fluid ">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
-                                <a className="nav-link active" aria-current="page" href="#" >Home</a>
-                                <a className="nav-link" href="#" >Features</a>
-                                <a className="nav-link" href="#" >Pricing</a>
+                <div className="row">
+                    <div className="offset-2">
+                        <img src="/img/insta.png" height="70" alt=""/>
+                    </div>
+                    <nav className="offset-3 navbar navbar-expand-lg">
+                        <div className="container-fluid ">
+                            <a className="navbar-brand" href="#">Navbar</a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <div className="navbar-nav">
+                                    <a className="nav-link active" aria-current="page" href="#" >Home</a>
+                                    <a className="nav-link" href="#" >Features</a>
+                                    <a className="nav-link" href="#" >Pricing</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
             </div>
         );
     }
